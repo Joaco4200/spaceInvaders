ee736752d3284e1f683fa13b55d896d6d1922ed8
Joaco4200 <joaquinscarone1012@gmail.com>
2024-07-22 15:19:51 -0300

    creatEnemys

---
+++ EnemyController.js
@@ -0,0 +1,30 @@
+export default class EnemyController {
+    enemyMap = [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [2, 2, 2, 3, 3, 3, 3, 2, 2, 2],
+        [2, 2, 2, 3, 3, 3, 3, 2, 2, 2],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+    ];
+    enemyRows = [];
+
+    constructor(canvas){
+        this.canvas = canvas;
+        this.createEnemys();
+    }
+
+    draw(ctx) {}
+
+    createEnemys() {
+        this.enemyMap.forEach((row,rowIndex) => {
+            this.enemyRows[rowIndex] = [];
+            row.forEach((enemyNumber, enemyIndex) => {
+                if(enemyNumber > 0){
+                    this.enemyNumber[rowIndex].push(new Enemy(enemyIndex * 50, this.enemyRows * 35, enemyNumber))
+                }
+            });
+        });
+    }
+}
+
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import EnemyController from "./EnemyController";
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
@@ -7,8 +8,11 @@ canvas.height = 600;
 const background = new Image();
 background.src = "images/space.png";
 
+const enemyController = new EnemyController(canvas)
+
 function game() {
     ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+    enemyController.draw(ctx);
 }
 
 setInterval(game, 1000/60)
